62eb6d87d584a68e79a23c15fe64aa9982867098
JustinXWT <73164010+JustinXWT@users.noreply.github.com>
2021-05-19 04:06:03 +0800

    Add files via upload

--- Client1/client_dynamic2.py
+++ Client1/client_dynamic2.py
@@ -3,8 +3,8 @@ import subprocess
 import time
 import os
 
-print("Running SIDH cipher")
+print("Running dragonfly cipher")
 while True:
-    os.system('python3 sidh_cipher_client.py')
+    os.system('python3 dragonfly_cipher_client.py')
     # time.sleep(5)
     # os.remove('secret.key.hacklab')
