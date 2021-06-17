a643669335fe5c7e33d22f86b7e8b04853d5da43
JoshuaKhooWX <73164041+JoshuaKhooWX@users.noreply.github.com>
2021-06-18 01:06:25 +0800

    Update sidh_private_keygen.py

--- Keygen/sidh_private_keygen.py
+++ Keygen/sidh_private_keygen.py
@@ -1038,7 +1038,7 @@ class ClientThread(threading.Thread):
             print ("Getting keys...\n")
             lock.acquire()
             
-            encryptDecrypt_start = time.perf_counter()
+            encrypt_start = time.perf_counter()
             print("Printing secret key...\n")
             secret_key = "secret.key"
 
@@ -1069,12 +1069,12 @@ class ClientThread(threading.Thread):
             s.close()
             t.close()
             
-            encryptDecrypt_stop = time.perf_counter()
+            encrypt_stop = time.perf_counter()
             #writing time taken to generate shared key between keygen and client
             KeyExchangeTiming = open('time.txt', 'a')
-            encryptDecrypt_time_total = round((encryptDecrypt_stop - encryptDecrypt_start), 3)
+            encrypt_time_total = round((encrypt_stop - encrypt_start), 3)
             KeyExchangeTiming.write('\nTotal Time Taken to Encryption/Decryption of keys for' + str(self.connection) + ': ')
-            KeyExchangeTiming.write(str(encryptDecrypt_time_total))
+            KeyExchangeTiming.write(str(encrypt_time_total))
             KeyExchangeTiming.write(str('--------------------------------------------------'))
             KeyExchangeTiming.close()
             
